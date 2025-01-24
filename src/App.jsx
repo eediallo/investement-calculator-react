dcb35b113e86329a4f284f0cf1b117b4c9a9225d
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-24 14:01:40 +0000

    create Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
 import Header from "./Header";
 
+function Input({ inputType, label }) {
+  return (
+    <div id="user-input">
+      <label htmlFor={label}>{label}</label>
+      <input type={inputType} id={label} />
+    </div>
+  );
+}
+
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="input-group">
+        <div>
+          <Input label="Initial Investment" inputType="text" />
+          <Input label="Expected Return" inputType="number" />
+        </div>
+        <div>
+          <Input label="Annual Investment" inputType="text" />
+          <Input label="Duration" inputType="text" />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
