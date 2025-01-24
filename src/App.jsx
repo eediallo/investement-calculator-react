265f4dfa37500d962608c3916a51febbb2d6adf5
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-24 18:04:13 +0000

    remove Result component and convert newValue to a number

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,7 @@
 import { useState } from "react";
 import Header from "./Header";
-import { calculateInvestmentResults } from "./util/investment";
-import { formatter } from "./util/investment";
 import UserInput from "./components/UserInput";
-
-function Result({ annualData, totalInterest }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Investment Value</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        {annualData.map((data, index) => (
-          <tr key={index}>
-            <td>{data.year}</td>
-            <td>{formatter.format(data.valueEndOfYear)}</td>
-            <td>{formatter.format(data.interest)}</td>
-            <td>
-              {formatter.format(
-                totalInterest && (totalInterest += data.interest)
-              )}
-            </td>
-            <td>{formatter.format(data.annualInvestment)}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
+import Results from "./components/Results";
 
 function App() {
   const [userInput, setUserInput] = useState({
@@ -47,7 +15,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: +newValue,
       };
     });
   }
@@ -55,6 +23,7 @@ function App() {
     <>
       <Header />
       <UserInput onChange={handleChange} userInput={userInput} />
+      <Results input={userInput} />
     </>
   );
 }
