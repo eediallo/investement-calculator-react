50683b7d44e04b0bb3f318e7a9a8838774143813
[Elhadj Abdoul Diallo] <[elhhabdouldiallo@gmail.com]>
2025-01-24 17:23:04 +0000

    refactor by creatiing UserInput component

--- src/App.jsx
+++ src/App.jsx
@@ -2,40 +2,9 @@ import { useState } from "react";
 import Header from "./Header";
 import { calculateInvestmentResults } from "./util/investment";
 import { formatter } from "./util/investment";
+import UserInput from "./components/UserInput";
 
-function Input({ inputType, label, initialValue, onChangeSelect }) {
-  return (
-    <div id="user-input">
-      <label htmlFor={label}>{label}</label>
-      <input
-        type={inputType}
-        id={label}
-        value={initialValue}
-        onChange={onChangeSelect}
-      />
-    </div>
-  );
-}
-
-function Result() {
-  const initialData = [
-    {
-      initialInvestement: 15000,
-      annualInvestment: 900,
-      expectedReturn: 6,
-      duration: 10,
-    },
-  ];
-
-  const annualData = calculateInvestmentResults({
-    initialInvestment: initialData[0].initialInvestement,
-    annualInvestment: initialData[0].annualInvestment,
-    expectedReturn: initialData[0].expectedReturn,
-    duration: initialData[0].duration,
-  });
-
-  let totalInterest = 0;
-
+function Result({ annualData, totalInterest }) {
   return (
     <table>
       <thead>
@@ -53,7 +22,11 @@ function Result() {
             <td>{data.year}</td>
             <td>{formatter.format(data.valueEndOfYear)}</td>
             <td>{formatter.format(data.interest)}</td>
-            <td>{formatter.format((totalInterest += data.interest))}</td>
+            <td>
+              {formatter.format(
+                totalInterest && (totalInterest += data.interest)
+              )}
+            </td>
             <td>{formatter.format(data.annualInvestment)}</td>
           </tr>
         ))}
@@ -63,48 +36,10 @@ function Result() {
 }
 
 function App() {
-  const [initialValue, setInitialValue] = useState(null);
-  function handleChange(e) {
-    setInitialValue(e.target.value);
-  }
-  // const [annualData, setAnnualData] = useState()
   return (
     <>
       <Header />
-      <div className="input-group">
-        <div>
-          <Input
-            label="Initial Investment"
-            inputType="text"
-            value={initialValue}
-            onChangeSelect={handleChange}
-          />
-          <Input
-            label="Expected Return"
-            inputType="number"
-            value={initialValue}
-            onChangeSelect={handleChange}
-          />
-        </div>
-        <div>
-          <Input
-            label="Annual Investment"
-            inputType="text"
-            value={initialValue}
-            onChangeSelect={handleChange}
-          />
-          <Input
-            label="Duration"
-            inputType="text"
-            value={initialValue}
-            onChangeSelect={handleChange}
-          />
-        </div>
-      </div>
-
-      <div id="result">
-        <Result />
-      </div>
+      <UserInput />
     </>
   );
 }
---
+++ src/components/UserInput.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+export default function UserInput() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 1000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue,
+      };
+    });
+  }
+  return (
+    <section id="user-input">
+      <div className="user-group">
+        <p>
+          <label htmlFor="Initial Inverstment">Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(e) => handleChange("initialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label htmlFor="Annual Inverstment">Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(e) => handleChange("annualInvestment", e.target.value)}
+          />
+        </p>
+
+        <p>
+          <label htmlFor="Expected return">Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(e) => handleChange("expectedReturn", e.target.value)}
+          />
+        </p>
+
+        <p>
+          <label htmlFor="Duration">Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(e) => handleChange("duration", e.target.value)}
+          />
+        </p>
+      </div>
+    </section>
+  );
+}
